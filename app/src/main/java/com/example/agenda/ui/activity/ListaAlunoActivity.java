6e322f05a1018e48f14f021e86e50cc8267770d0
guisedassari <guisedassari@gmail.com>
2020-02-04 17:19:49 -0300

    First commit

---
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
@@ -0,0 +1,56 @@
+package com.example.agenda.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agenda.R;
+import com.example.agenda.dao.AlunosDAO;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ListaAlunoActivity extends AppCompatActivity {
+
+    public static final String TITULO_APPBAR = "Lista de Alunos";
+    private final AlunosDAO dao = new AlunosDAO();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_aluno);
+        setTitle(TITULO_APPBAR);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        configuraLista();
+    }
+
+    private void configuraLista() {
+        ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listaview);
+        configuraFabNovoAluno();
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
+    }
+
+    private void configuraFabNovoAluno() {
+        FloatingActionButton botaoNovoAluno = findViewById(R.id.activity_lista_aluno_fab_novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abreFormularioAlunoActivity();
+            }
+        });
+    }
+
+    private void abreFormularioAlunoActivity() {
+        startActivity(new Intent(this, FormularioAlunoActivity.class));
+    }
+}
