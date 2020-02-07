158239105cf307a790897df97cbe4b007be5989e
guisedassari <guisedassari@gmail.com>
2020-02-07 17:57:54 -0300

    continuaçao da implementação da funcionalidade de edição

--- app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
+++ app/src/main/java/com/example/agenda/ui/activity/ListaAlunoActivity.java
@@ -2,7 +2,9 @@ package com.example.agenda.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -12,7 +14,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda.R;
 import com.example.agenda.dao.AlunosDAO;
+import com.example.agenda.model.Aluno;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class ListaAlunoActivity extends AppCompatActivity {
 
@@ -25,26 +30,28 @@ public class ListaAlunoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_aluno);
         setTitle(TITULO_APPBAR);
 
+        dao.salva(new Aluno("Guilherme", "993472745", "guisedassari@gmail.com"));
+        dao.salva(new Aluno("Priscila", "999999999", "priscila@gmail.com"));
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         configuraLista();
     }
 
     private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listaview);
         configuraFabNovoAluno();
-
-        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, dao.todos()));
-
-        listaDeAlunos.setOnClickListener(new View.OnClickListener() {
-
+        final List<Aluno> alunos = dao.todos();
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(ListaAlunoActivity.this, "Aluno clicado", Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Aluno alunoEscolhido = alunos.get(position);
+//                Log.i("aluno", "" + alunoEscolhido);
+                Intent vaiParaFormularioActivity = new Intent(ListaAlunoActivity.this, FormularioAlunoActivity.class);
+                startActivity(vaiParaFormularioActivity);
             }
         });
     }
